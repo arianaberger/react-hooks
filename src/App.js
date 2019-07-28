8eb0efb7feb968a5721ecdc8695d42685ca2691a
Ariana <ariana.berger@gmail.com>
2019-07-28 19:59:22 -0400

    Add basic state hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import { useForm } from "./useForm";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return(
+    <div>
+      The count is currently: {count}
+    </div>
+  );
+};
 
 export default App;
