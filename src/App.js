cb66250426a15280b52306d193ca70700d198c95
Ariana <ariana.berger@gmail.com>
2019-07-28 20:32:17 -0400

    Add two objects to state hook

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import './App.css';
 import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [{count1, count2}, setCount] = useState({count1: 10, count2: 20});
 
   return(
     <div className="app">
+      <h3 className="cursive">Let's learn some React hooks</h3>
       <div className="wrapper">
-        <div className="counter">
-          The count is currently: {count}
-        </div>
-        <div className="counter">
-          <button onClick={() => setCount(count + 1)}>Click me!</button>
-        </div>
+          <h2>The first count is currently: {count1}</h2>
+          <h2>The second count will stay at: {count2}</h2>
+          <button
+            onClick={() =>
+              setCount(currentState => ({
+                ...currentState,
+                count1: currentState.count1 + 1
+              }))
+            }
+          >
+            Click me!
+          </button>
       </div>
     </div>
   );
