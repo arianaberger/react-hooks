09c7f37117091456bc7791e6edfb1f908679996d
Ariana <ariana.berger@gmail.com>
2019-08-08 15:32:09 -0400

    Streamline timeout code for fact fetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -3,20 +3,15 @@ import { useEffect, useState } from 'react';
 export const useFetch = (url) => {
   const [data, setData] = useState({fact: null, loading: true});
 
-  function sleeper(ms) {
-    return function(x) {
-      return new Promise(resolve => setTimeout(() => resolve(x), ms));
-    };
-  }
-
   useEffect(() => {
     setData({fact: null, loading: true});
     fetch(url)
       .then(x => x.text())
-      .then(sleeper(1500))
       .then(y => {
-        setData({fact: y, loading: false})
-      })
+        setTimeout(() => {
+          setData({fact: y, loading: false});
+        }, 1500)
+      });
   }, [url, setData])
 
   return data
