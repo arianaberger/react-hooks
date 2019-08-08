71c89540353d82dac5b4d65926cd917ece9f51cf
Ariana <ariana.berger@gmail.com>
2019-08-08 14:30:20 -0400

    Create component to fetch number facts

--- src/Cat.js
+++ src/Cat.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useFetch } from './useFetch';
 
 const Cat = () => {
+
+  useFetch('http://numbersapi.com/43/trivia')
+
   return(
     <div className="image">
       <a href="https://giphy.com/gifs/cat-party-hard-rainbow-shit-ZIcOEgfPoRoe4">
